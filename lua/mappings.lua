c9c70123491f08458a846e9230866a02d0cf6e3d
Yakumo-zi <yakumozi1110@gmail.com>
2025-10-11 10:37:01 +0800

    nvim: add find diagnostics keymap

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -15,6 +15,7 @@ map("n", "<leader>fo", "<cmd> Telescope oldfiles <CR>")
 map("n", "<leader>fw", "<cmd> Telescope live_grep <CR>")
 map("n", "<leader>gt", "<cmd> Telescope git_status <CR>")
 map("n", "<leader>gr", "<cmd> Telescope lsp_references <CR>")
+map("n", "<leader>fd", "<cmd> Telescope diagnostics <CR>")
 
 -- bufferline, cycle buffers
 map("n", "<Tab>", "<cmd> BufferNext <CR>")
