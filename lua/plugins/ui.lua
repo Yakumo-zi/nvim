5c9b511b5d185de8514249973fbe68a2a7abe18d
Yakumo-zi <yakumozi1110@gmail.com>
2025-10-15 11:29:03 +0800

    nvim:增加 git_blame

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -62,4 +62,14 @@ return {
       vim.notify = require("notify")
     end,
   },
+  {
+    "f-person/git-blame.nvim",
+    event = "VeryLazy",
+    opts = {
+      enabled = true,
+      message_template = " <summary> • <date> • <author> • <<sha>>",
+      date_format = "%Y-%m-%d %r",
+      virtual_text_column = 1,
+    },
+  },
 }
