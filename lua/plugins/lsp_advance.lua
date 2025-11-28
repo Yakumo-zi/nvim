be704c97ebf4816f4c0706517a874918c1a4e23a
Yakumo-zi <yakumozi1110@gmail.com>
2025-11-28 14:57:52 +0800

    remove go.nvim, make config keep simple

--- lua/plugins/lsp_advance.lua
+++ lua/plugins/lsp_advance.lua
@@ -13,18 +13,6 @@ return {
       vim.diagnostic.config({ virtual_text = false }) -- Disable default virtual text
     end,
   },
-
-  {
-    "ray-x/go.nvim",
-    dependencies = { -- optional packages
-      "ray-x/guihua.lua",
-      "neovim/nvim-lspconfig",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    config = function() require("plugins.configs.go").setup() end,
-    ft = { "go", "gomod" },
-    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-  },
   {
     "rachartier/tiny-code-action.nvim",
     dependencies = {
